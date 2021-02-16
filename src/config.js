86e034842060a7cf14fbbff92d77254d12385c54
soymikey <soymikey@github.com>
2021-02-16 11:43:16 +0800

    修改 project config 文件

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@ let baseWsURL
 const env = process.env.NODE_ENV
 
 if (env === 'development') {
-    baseURL = 'http://192.168.1.114:3100'
-    baseWsURL = 'ws://192.168.1.114:3101'
+    baseURL = 'http://192.168.1.117:3100'
+    baseWsURL = 'ws://192.168.1.117:3101'
     //  baseURL = 'https://streetmusic.migaox.com'
     //  baseWsURL = 'wss://streetmusicws.migaox.com'
 } else {
